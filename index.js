14cbafd7829671a53148d8742332a3599931196b
elvladi15 <53945940+elvladi15@users.noreply.github.com>
2021-04-23 21:09:38 -0400

    login (UI)

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const port = 3000;
 const express = require("express");
+
+//requerir archivos con las rutas de la app
 const routes = require("./routes");
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
+//para leer archivos de css ubicados en views/public/assets
+app.use('/public', express.static('views/public'));
+
+//usar rutas
 app.use(routes);
+
 app.listen(port, () => {
     console.log("Servidor corriendo en el puerto ", port);
 });
--- routes.js
+++ routes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const app = express();
 
-router.get("/login", (req, res) => {
-    res.render("login.ejs");
+router.get("/", (req, res) => {
+    res.render("login/index.ejs");
 });
 
 module.exports = router;
