1e6ead7c18fed598807c18b9ef0c115cd3742d11
Víctor A. Montero <vamj809@users.noreply.github.com>
2021-04-25 14:48:31 -0400

    Added Basic Views Structure and routes

--- routes.js
+++ routes.js
@@ -11,5 +11,14 @@ router.get("/register-page",(req,res)=>{
 router.get("/dashboard",(req,res)=>{
     res.render("dashboard.ejs");
 })
+router.get("/payments",(req,res)=>{
+    res.render("payments.ejs");
+})
+router.get("/clients",(req,res)=>{
+    res.render("clients.ejs");
+})
+router.get("/profile",(req,res)=>{
+    res.render("profile.ejs");
+})
 
 module.exports = router;
