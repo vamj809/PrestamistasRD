454792b98800d265b54dd3142f9e01d45eeb2321
Víctor A. Montero <vamj809@users.noreply.github.com>
2021-04-25 01:30:27 -0400

    Added TO-DO

--- models/model.prestamistas.js
+++ models/model.prestamistas.js
@@ -1,16 +1,35 @@
-import conn from '../index';
 import config from '../config/api.config';
 const URL_Params = '/Prestamistas';
 
-var Prestamista = function(prestamista) {
-    //TODO: Crear Prestamista
-    //forma: this.atributo = prestamista.atributo
+//TO-DO: Abstraer el module.exports para uso genérico de los distintos modelos y 
+//       poder facilitar la extensión del mismo. #SOLID.
+
+var Prestamista = function(_prestamista) {
+    //TODO: Crear Prestamista [Actualizar atributos en base a definido en arquitectura]
+    this._id = _prestamista._id;
+    this.userName = _prestamista.userName;
+    this.displayName = _prestamista.displayName;
+    this.email = _prestamista.email;
+    this.passwordHash = _prestamista.passwordHash;
+    this.passwordSalt = _prestamista.passwordSalt;
+    this.lastAccessDate = _prestamista.lastAccessDate;
+    this.createdAt = _prestamista.createdAt;
+    this.createdBy = _prestamista.createdBy;
+    this.attemptFails = _prestamista.attemptFails;
 };
 
 module.exports = {
+    Auth: (email, password) => config.ApiConsumer({
+        method:"POST",
+        url: config.connectionString + URL_Params + '/auth',
+        params: {
+            user: email,
+            password: password
+        }
+    }),
     Create: (name, lastname, email, password) => config.ApiConsumer({
         method:"POST",
-        url: config.connectionString + URL_Params,
+        url: config.connectionString + URL_Params + '/create',
         params: {
             displayname: lastname + ', ' + name,
             email: email,
