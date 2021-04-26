00b1afe7b3ea507ce63388e4d92af9ac5a2f2a60
Víctor A. Montero <vamj809@users.noreply.github.com>
2021-04-26 06:29:46 -0400

    Added Creacion de Prestamos

--- controllers/controller.prestamos.js
+++ controllers/controller.prestamos.js
@@ -3,10 +3,32 @@ const basecontroller = require('./controller.base');
 const URL_Params = basecontroller.URL.Prestamos;
 const Prestamo = {}
 
-Prestamo.Create = async function Create(prestamo){
-    let res = await basecontroller.Create(URL_Params, prestamo);
-    return res;
-}
+Prestamo.Create = async (req, res) => {
+    try {
+            var _prestamo = {
+            idCliente: req.body.idCliente,
+            monto: req.body.monto,
+            cuotas: req.body.cuotas,
+            valorCuotas: req.body.valorCuotas,
+            interes: req.body.interes,
+            montoRestante: req.body.monto,
+            fechaPrestamo: req.body.fechaPrestamo,
+            fechaLimite: req.body.fechaLimite
+        }
+        let response = await basecontroller.Create(URL_Params, _prestamo);
+        
+        if(response.data.success){
+            res.redirect('/page');
+        } else{
+            throw Error(res.data);
+        }
+        
+        return response;
+    } catch(error) {
+        console.log(error.message);
+        res.send("Error del servidor. Vuelva a intentarlo: " + error.message);
+    }
+} 
 
 // Prestamo.Get = async function Get(id){
 //     let res = await basecontroller.Read(URL_Params, id);
@@ -15,18 +37,18 @@ Prestamo.Create = async function Create(prestamo){
 
 
 Prestamo.GetAll = async function GetAll(){
-    let res = await basecontroller.ReadAll(URL_Params);
-    return res;
+    let response = await basecontroller.ReadAll(URL_Params);
+    return response;
 }
 
-Prestamo.Update = async function Update(prestamo){
-    let res = await basecontroller.Update(URL_Params, prestamo);
-    return res;
+Prestamo.Update = async (req, res) => {
+    let response = await basecontroller.Update(URL_Params, req.body);
+    return response;
 }
 
-Prestamo.Delete = async function Delete(id){
-    let res = await basecontroller.Update(URL_Params, id);
-    return res;
+Prestamo.Delete = async (req, res) => {
+    let response = await basecontroller.Delete(URL_Params, req.body.id);
+    return response;
 }
 
 module.exports = Prestamo;
